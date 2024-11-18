f945982050886139a005a1a45abbe3585f3743e3
Borislav Pantaleev <beastafk@gmail.com>
2024-11-18 23:30:16 +0200

    Add preferred theme colors to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import { Avatar, Box, Link, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
 
 const Footer = () => {
   const [version, setVersion] = useState<string | null>(null);
+  const theme = useTheme();
+
   useEffect(() => {
     const version = document.getElementById("js-version")?.textContent;
     if (version) {
@@ -17,23 +20,32 @@ const Footer = () => {
       zIndex: 100,
       bottom: 0,
       width: '100%',
-      bgcolor: "#eee",
+      bgcolor: theme.palette.background.default,
+      color: theme.palette.primary.contrastText,
       borderTop: '1px solid',
-      borderColor: '#ddd',
+      borderColor: theme.palette.divider,
+      fontSize: '0.89rem',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'start',
       p: 1,
+      gap: '10px'
     }}>
-    <Typography variant="body2" component="div">
       <Avatar src="./images/logo.webp" sx={{ width: "1rem", height: "1rem", display: "inline-block", verticalAlign: "sub" }} />
-      <Link sx={{ color: "#888", textDecoration: 'none' }} href="https://github.com/etkecc/synapse-admin" target="_blank">
+      <Link sx={{ display: 'inline-block', color: theme.palette.info.main, textDecoration: 'none' }} href="https://github.com/etkecc/synapse-admin" target="_blank">
         Synapse Admin
-      </Link> <Link href={`https://github.com/etkecc/synapse-admin/releases/tag/`+version} target="_blank">
-        <span style={{ fontWeight: 'bold', color: "#000" }}>{version}</span>
-      </Link> <Link sx={{ color: "#888", textDecoration: 'none' }} href="https://etke.cc/?utm_source=synapse-admin&utm_medium=footer&utm_campaign=synapse-admin" target="_blank">
+      </Link>
+      <Link sx={{ display: 'inline-block', color: theme.palette.grey[600] }} href={`https://github.com/etkecc/synapse-admin/releases/tag/`+version} target="_blank">
+        <span style={{ fontWeight: 'bold' }}>{version}</span>
+      </Link>
+      <Link sx={{  textDecoration: 'none', color: theme.palette.info.main }} href="https://etke.cc/?utm_source=synapse-admin&utm_medium=footer&utm_campaign=synapse-admin" target="_blank">
         by etke.cc
-      </Link> <Link sx={{ color: "#888", textDecoration: 'none' }} href="https://github.com/awesome-technologies/synapse-admin" target="_blank">
+      </Link>
+      <Link sx={{ textDecoration: 'none', color: theme.palette.grey[600] }} href="https://github.com/awesome-technologies/synapse-admin" target="_blank">
         (originally developed by Awesome Technologies Innovationslabor GmbH).
-      </Link> <Link sx={{ fontWeight: 'bold', color: "#000", textDecoration: 'none' }} href="https://matrix.to/#/#synapse-admin:etke.cc" target="_blank">#synapse-admin:etke.cc</Link>
-    </Typography>
+      </Link>
+      <Link sx={{ fontWeight: 'bold', color: theme.palette.info.main,textDecoration: 'none' }} href="https://matrix.to/#/#synapse-admin:etke.cc" target="_blank">#synapse-admin:etke.cc</Link>
   </Box>
   );
 };
